835772d755ed135628335c05180f7cab7d72b718
Olegjan <oleg-golota@yandex.ru>
2017-06-29 16:46:15 +0300

    Update MoveBulletObjectScript.cs

--- MoveBulletObjectScript.cs
+++ MoveBulletObjectScript.cs
@@ -1,14 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
-//Этот скрипт перемещает Снаряд в место расположения Астероида, и после их столкновения уничтожает Астероид или Button, сам снаряд и Canvas Астероида.
 public class MoveBulletObjectScript : MonoBehaviour {
 	
-	public Transform PlaceAsteroidButton; // Место положения текстуры или кнопки Button в Астероиде
-	public float BulletSpeed; //Скорость с которой снаряд двигаеться к Астероиду
-	public GameObject MainAnimationScriptObject; // Объект на котором весит Основной скрипт для утановки видимости и позиций объектов
+	public Transform PlaceAsteroidButton;
+	public float BulletSpeed; 
+	public GameObject MainAnimationScriptObject; 
 
 	void Update () {
-		//если между Снарядом и Астероидом будет такое растояние,то оба эти объекта будут уничтожены 
 		if (Vector3.Distance(transform.position, PlaceAsteroidButton.position) < 0.5f) { 
 			MainAnimationScriptObject.SetActive (true);
 		}			
