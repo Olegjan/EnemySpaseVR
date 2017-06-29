b4ebcbce2818625f183e0eb7f248f1b4e6822011
Olegjan <oleg-golota@yandex.ru>
2017-06-29 16:23:33 +0300

    Add files via upload

---
+++ MoveBulletObjectScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+//Этот скрипт перемещает Снаряд в место расположения Астероида, и после их столкновения уничтожает Астероид или Button, сам снаряд и Canvas Астероида.
+public class MoveBulletObjectScript : MonoBehaviour {
+	
+	public Transform PlaceAsteroidButton; // Место положения текстуры или кнопки Button в Астероиде
+	public float BulletSpeed; //Скорость с которой снаряд двигаеться к Астероиду
+	public GameObject MainAnimationScriptObject; // Объект на котором весит Основной скрипт для утановки видимости и позиций объектов
+
+
+	void Start () {
+	}
+
+	void Update () {
+		//transform.Translate (transform.forward * Time.deltaTime * BulletSpeed);
+		//если между Снарядом и Астероидом будет такое растояние,то оба эти объекта будут уничтожены 
+		if (Vector3.Distance(transform.position, PlaceAsteroidButton.position) < 0.5f) { 
+			MainAnimationScriptObject.SetActive (true);
+		}			
+		else 
+			transform.position = Vector3.MoveTowards(transform.position, PlaceAsteroidButton.position, Time.deltaTime * BulletSpeed);
+	}
+}
---
+++ ScriptForAsteroidCanvas.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptForAsteroidCanvas : MonoBehaviour {
+	public float AsteroidSpeed1 = 2f; // скорость приближения Canvas и Астероида к земле
+
+
+	void Start () {
+		Destroy (gameObject, 150f);
+	}
+
+	void Update () {
+		if (Vector3.Distance(transform.position, new Vector3(0, 0, 0)) < 0.19f)//Условие которое сработает когда до объекта в public GameObject Bullet будет меньше чем 0.03f
+		{
+		//	Destroy (gameObject);
+		}
+
+		else // с помощью этого условия данный объект или Астероид движиться к Земле или EarthEndPoint с определённой скоростью, которая указана в AsteroidSpeed
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 0, 0), Time.deltaTime * AsteroidSpeed1); 	
+	}
+
+}
