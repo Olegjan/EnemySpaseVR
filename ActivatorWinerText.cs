e8f8fe32b48d92da9955f200cb7c063c49b0f3d7
Olegjan <oleg-golota@yandex.ru>
2017-06-29 16:13:55 +0300

    Add files via upload

---
+++ ActivatorWinerText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;//Этот скрипт через указаное время создаёт затемнение сцены перед переходом в другую сцену, 
+using System.Collections;// запускает анимацию слова louding и загружает Следующюю сцену для Выигравшего игрока
+
+public class ActivatorWinerText : MonoBehaviour {
+	//public GameObject Winer;
+	public float ActivatorTime = 160f; // время через которое начнёт выполняться данный скрипт
+	public string sceneName; // иногда здесь можно указать название сцены и мы перейдём к этой сцене
+	public GameObject FadeInScene; // Объект который создаёт затемнение в сцене перед переходом в другую сцену
+	public GameObject AnimLoad; // Объект который отвечает за анимацию слова Louding
+
+	void Start () {
+		StartCoroutine ("TestTimer");//функция запускающяя таймер
+		//gameObject.SetActiveRecursively (true); //Рекурсивная активация всей групы дочерних объектов
+	}
+
+	IEnumerator TestTimer(){
+		Debug.Log ("Go my test");
+		yield return new WaitForSeconds (ActivatorTime);
+		FadeInScene.SetActive (true);//Активация объекта который создаёт затемнение в сцене перед переходом в другую сцену
+		yield return new WaitForSeconds (2f);
+		AnimLoad.SetActive (true);//Активация объекта который отвечает за анимацию слова Louding
+		Application.LoadLevel(sceneName); //Переход в указаную сцену
+		Debug.Log ("Stop my test");
+	}
+}
---
+++ TestNewOB.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;//Это ключевой скрипт текущей игры
+using System.Collections;
+
+public class TestNewOB : MonoBehaviour {
+	public GameObject AsteroidCanvas; // Объект 
+	public GameObject AnimationExplosion; // Объект на котором висит анимация взрыва
+	public GameObject MainAnimationScriptObject; // Объект на котором весит Основной скрипт для утановки видимости и позиций объектов
+	public GameObject PanelButton; // Объект на котором висит текстура Астероида
+	public GameObject SphereBullet; // Объет Снаряда
+	public GameObject NextCreateAsteroid; //следующий астероид который будет активирован после деактивации текощего создателя астероидов
+	public GameObject NextAsteroidCreateForCopyNew;
+	public GameObject LifeTimer;
+	public Transform PlaceAsteroidCanvas; // Место расположение Канваса Астероида
+	public Transform PlaceCreateAsteroid; // Место создания первого Астероида
+	public Transform PlaceCamera; // Место расположения камеры 
+	public Transform PlaceSphereBullet; // Место расположения Снаряда
+
+	void OnEnable(){
+		StartCoroutine ("TestTimer");
+	}
+	IEnumerator TestTimer() {
+		Debug.Log ("Go my test");
+		SphereBullet.SetActive (false); // с помощью этой строки деактивируеться объект Снаряда
+		PanelButton.SetActive (false); // с помощью этой строки деактивируеться объект Panel или Панель астероида
+		AnimationExplosion.SetActive (true); // с помощью этой строки активируеться объект взрыва астероида
+		yield return new WaitForSeconds (0.1f);
+		PlaceSphereBullet.position = PlaceCamera.position; //с помощью этой строки Снаряд перемещаеться в месторасположения камеры
+		yield return new WaitForSeconds (2f);
+		AsteroidCanvas.SetActive (false); // с помощью этой строки Астероид становиться невидимым
+		yield return new WaitForSeconds (0.1f);
+		PlaceAsteroidCanvas.position = PlaceCreateAsteroid.position; // с помощью этой строки астероид перемещаеться в место создания Астероидов
+		if (LifeTimer !=null){
+		MainAnimationScriptObject.SetActive (false); // с помощью этой строки деактивируеться объект скрипта и сам этот скрипт
+		}
+		//if (LifeTimer =null){
+		//	yield return new WaitForSeconds (0.2f);
+		//	MainAnimationScriptObject.SetActive (false); // с помощью этой строки деактивируеться объект скрипта и сам этот скрипт
+		//}
+		yield return new WaitForSeconds (0.2f);
+		Debug.Log ("Stop my test");
+	}
+	void OnDisable(){
+		AnimationExplosion.SetActive (false); // с помощью этой строки активируеться объект взрыва астероида
+		PanelButton.SetActive (true); // с помощью этой строки активируеться объект Panel или Панель астероида
+		NextCreateAsteroid.SetActive (true);
+		NextAsteroidCreateForCopyNew.SetActive (true);
+	}
+}
