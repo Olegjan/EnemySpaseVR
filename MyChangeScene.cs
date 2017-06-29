68da6a3902aa1f60acb4eae4706fba70e59e614d
Olegjan <oleg-golota@yandex.ru>
2017-06-29 16:17:08 +0300

    Update MyChangeScene.cs

--- MyChangeScene.cs
+++ MyChangeScene.cs
@@ -3,13 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class MyChangeScene : MonoBehaviour {
-	//public GameObject FadeInScene;// Объект который создаёт затенение сцены
-	//public GameObject AnimLoad;// Объект на котором находиться анимация слова Louding
 	public string sceneName; // иногда здесь можно указать название сцены и мы перейдём к этой сцене
 
 	public void ChangeScene(string sceneName) {
-		//FadeInScene.SetActive (true);//Активация объекта который создаёт затенение сцены
-		//AnimLoad.SetActive (true);//Активация объекта на котором находиться анимация слова Louding
 		Application.LoadLevel(sceneName);//сцена куда мы будем перенаправлены
 	}
 }
