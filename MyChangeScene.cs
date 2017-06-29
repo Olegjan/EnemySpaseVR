b7d4cc01693b60ae328aee3f5f4492bca3b9c5e8
Olegjan <oleg-golota@yandex.ru>
2017-06-29 16:47:03 +0300

    Update MyChangeScene.cs

--- MyChangeScene.cs
+++ MyChangeScene.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;// Скрипт с помощью которого происходит переход в указанную сцену
+﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
 public class MyChangeScene : MonoBehaviour {
-	public string sceneName; // иногда здесь можно указать название сцены и мы перейдём к этой сцене
+	public string sceneName;
 
 	public void ChangeScene(string sceneName) {
-		Application.LoadLevel(sceneName);//сцена куда мы будем перенаправлены
+		Application.LoadLevel(sceneName);
 	}
 }
